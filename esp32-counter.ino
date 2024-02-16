f934390efd2e183354c93a3ef9c9a4093d95fe01
Alexander Efimov <efimov.alexander@gmail.com>
2024-02-16 22:35:07 +0100

    cleanup code

--- esp32-counter.ino
+++ esp32-counter.ino
@@ -21,28 +21,6 @@ Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, 
 #define SENSOR_PIN_LEFT 12
 #define SENSOR_PIN_MIDDLE 12
 #define SENSOR_PIN_RIGHT 12
-#define NUM_SAMPLES 10
-
-#define LOGO16_GLCD_HEIGHT 16
-#define LOGO16_GLCD_WIDTH  16
-static const unsigned char PROGMEM logo16_glcd_bmp[] =
-{ B00000000, B11000000,
-  B00000001, B11000000,
-  B00000001, B11000000,
-  B00000011, B11100000,
-  B11110011, B11100000,
-  B11111110, B11111000,
-  B01111110, B11111111,
-  B00110011, B10011111,
-  B00011111, B11111100,
-  B00001101, B01110000,
-  B00011011, B10100000,
-  B00111111, B11100000,
-  B00111111, B11110000,
-  B01111100, B11110000,
-  B01110000, B01110000,
-  B00000000, B00110000
-};
 
 /**
  * @brief abstract class controlling button state
@@ -108,7 +86,14 @@ public:
   }
 };
 
-class ThreeStateButtonWidget {
+class Widget {
+public:
+  virtual void reset() = 0;
+  virtual void update() = 0;
+  virtual void draw() = 0;
+}
+
+class ThreeStateButtonWidget: public Widget {
   int off_x;
   int off_y;
   const char *short_press_text;
@@ -129,18 +114,18 @@ public:
     state.reset();
   }
 
-  void reset() {
+  void reset() override{
     state.reset();
   }
 
-  void update() {
+  void update() override{
     bool pin_state = digitalRead(control_pin_no);
     int timestamp = esp_timer_get_time() / 1000;
     int event = state.updateState(timestamp, !pin_state);
     // todo callback on release
   }
 
-  void draw() {
+  void draw() override{
     // draw labels
     int s = state.getState();
     constexpr int second_part_offset = 0;
@@ -169,7 +154,7 @@ public:
   }
 };
 
-class TwoStateButtonWidget {
+class TwoStateButtonWidget: public Widget {
   int off_x;
   int off_y;
   const char *press_text;
@@ -188,18 +173,18 @@ public:
     state.reset();
   }
 
-  void reset() {
+  void reset() override{
     state.reset();
   }
 
-  void update() {
+  void update() override{
     bool pin_state = digitalRead(control_pin_no);
     int timestamp = esp_timer_get_time() / 1000;
     int event = state.updateState(timestamp, !pin_state);
     // todo callback on release
   }
 
-  void draw() {
+  void draw() override{
     int s = state.getState();
     display.setTextColor(SH110X_WHITE);
     int press_text_length = strlen(press_text) * 6;
@@ -210,6 +195,21 @@ public:
       display.drawFastHLine(off_x, off_y + 8, press_text_length, SH110X_WHITE);
   }
 };
+
+class CounterWidget {
+  int state;
+  int addition;
+public:
+  void reset() override {
+    // Do nothing?
+  }
+  void update() override {
+    
+  }
+  void draw() override {
+    
+  }
+}
 
 ThreeStateButtonWidget plus_minus_1;
 ThreeStateButtonWidget plus_minus_5;
@@ -225,10 +225,8 @@ void setup() {
   // Since the buffer is intialized with an Adafruit splashscreen
   // internally, this will display the splashscreen.
 
-  delay(250); // wait for the OLED to power up
-  display.begin(i2c_Address, true); // Address 0x3C default
- //display.setContrast (0); // dim display
-  //pinMode(SENSOR_PIN, OUTPUT);
+  delay(250);
+  display.begin(i2c_Address, true);
  
   display.display();
   delay(2000);
@@ -246,276 +244,4 @@ void loop() {
   plus_minus_5.draw();
   menu.draw();
   display.display();
-}
-
-//  // draw a single pixel
-//  display.drawPixel(10, 10, SH110X_WHITE);
-//  // Show the display buffer on the hardware.
-//  // NOTE: You _must_ call display after making any drawing commands
-//  // to make them visible on the display hardware!
-//  display.display();
-//  delay(2000);
-//  display.clearDisplay();
-//
-//  // draw many lines
-//  testdrawline();
-//  display.display();
-//  delay(2000);
-//  display.clearDisplay();
-//
-//  // draw rectangles
-//  testdrawrect();
-//  display.display();
-//  delay(2000);
-//  display.clearDisplay();
-//
-//  // draw multiple rectangles
-//  testfillrect();
-//  display.display();
-//  delay(2000);
-//  display.clearDisplay();
-//
-//  // draw mulitple circles
-//  testdrawcircle();
-//  display.display();
-//  delay(2000);
-//  display.clearDisplay();
-//
-//  // draw a SH110X_WHITE circle, 10 pixel radius
-//  display.fillCircle(display.width() / 2, display.height() / 2, 10, SH110X_WHITE);
-//  display.display();
-//  delay(2000);
-//  display.clearDisplay();
-//
-//  testdrawroundrect();
-//  delay(2000);
-//  display.clearDisplay();
-//
-//  testfillroundrect();
-//  delay(2000);
-//  display.clearDisplay();
-//
-//  testdrawtriangle();
-//  delay(2000);
-//  display.clearDisplay();
-//
-//  testfilltriangle();
-//  delay(2000);
-//  display.clearDisplay();
-//
-//  // draw the first ~12 characters in the font
-//  testdrawchar();
-//  display.display();
-//  delay(2000);
-//  display.clearDisplay();
-//
-//
-//
-//  // text display tests
-//  display.setTextSize(1);
-//  display.setTextColor(SH110X_WHITE);
-//  display.setCursor(0, 0);
-//  display.println("Failure is always an option");
-//  display.setTextColor(SH110X_BLACK, SH110X_WHITE); // 'inverted' text
-//  display.println(3.141592);
-//  display.setTextSize(2);
-//  display.setTextColor(SH110X_WHITE);
-//  display.print("0x"); display.println(0xDEADBEEF, HEX);
-//  display.display();
-//  delay(2000);
-//  display.clearDisplay();
-//
-//  // miniature bitmap display
-//  display.drawBitmap(30, 16,  logo16_glcd_bmp, 16, 16, 1);
-//  display.display();
-//  delay(1);
-//
-//  // invert the display
-//  display.invertDisplay(true);
-//  delay(1000);
-//  display.invertDisplay(false);
-//  delay(1000);
-//  display.clearDisplay();
-//
-//  // draw a bitmap icon and 'animate' movement
-//  testdrawbitmap(logo16_glcd_bmp, LOGO16_GLCD_HEIGHT, LOGO16_GLCD_WIDTH);
-//}
-//
-//
-//
-//
-//void testdrawbitmap(const uint8_t *bitmap, uint8_t w, uint8_t h) {
-//  uint8_t icons[NUMFLAKES][3];
-//
-//  // initialize
-//  for (uint8_t f = 0; f < NUMFLAKES; f++) {
-//    icons[f][XPOS] = random(display.width());
-//    icons[f][YPOS] = 0;
-//    icons[f][DELTAY] = random(5) + 1;
-//
-//    Serial.print("x: ");
-//    Serial.print(icons[f][XPOS], DEC);
-//    Serial.print(" y: ");
-//    Serial.print(icons[f][YPOS], DEC);
-//    Serial.print(" dy: ");
-//    Serial.println(icons[f][DELTAY], DEC);
-//  }
-//
-//  while (1) {
-//    // draw each icon
-//    for (uint8_t f = 0; f < NUMFLAKES; f++) {
-//      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, SH110X_WHITE);
-//    }
-//    display.display();
-//    delay(200);
-//
-//    // then erase it + move it
-//    for (uint8_t f = 0; f < NUMFLAKES; f++) {
-//      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, SH110X_BLACK);
-//      // move it
-//      icons[f][YPOS] += icons[f][DELTAY];
-//      // if its gone, reinit
-//      if (icons[f][YPOS] > display.height()) {
-//        icons[f][XPOS] = random(display.width());
-//        icons[f][YPOS] = 0;
-//        icons[f][DELTAY] = random(5) + 1;
-//      }
-//    }
-
-
-void testdrawchar(void) {
-  display.setTextSize(1);
-  display.setTextColor(SH110X_WHITE);
-  display.setCursor(0, 0);
-
-  for (uint8_t i = 0; i < 168; i++) {
-    if (i == '\n') continue;
-    display.write(i);
-    if ((i > 0) && (i % 21 == 0))
-      display.println();
-  }
-  display.display();
-  delay(1);
-}
-
-void testdrawcircle(void) {
-  for (int16_t i = 0; i < display.height(); i += 2) {
-    display.drawCircle(display.width() / 2, display.height() / 2, i, SH110X_WHITE);
-    display.display();
-    delay(1);
-  }
-}
-
-void testfillrect(void) {
-  uint8_t color = 1;
-  for (int16_t i = 0; i < display.height() / 2; i += 3) {
-    // alternate colors
-    display.fillRect(i, i, display.width() - i * 2, display.height() - i * 2, color % 2);
-    display.display();
-    delay(1);
-    color++;
-  }
-}
-
-void testdrawtriangle(void) {
-  for (int16_t i = 0; i < min(display.width(), display.height()) / 2; i += 5) {
-    display.drawTriangle(display.width() / 2, display.height() / 2 - i,
-                         display.width() / 2 - i, display.height() / 2 + i,
-                         display.width() / 2 + i, display.height() / 2 + i, SH110X_WHITE);
-    display.display();
-    delay(1);
-  }
-}
-
-void testfilltriangle(void) {
-  uint8_t color = SH110X_WHITE;
-  for (int16_t i = min(display.width(), display.height()) / 2; i > 0; i -= 5) {
-    display.fillTriangle(display.width() / 2, display.height() / 2 - i,
-                         display.width() / 2 - i, display.height() / 2 + i,
-                         display.width() / 2 + i, display.height() / 2 + i, SH110X_WHITE);
-    if (color == SH110X_WHITE) color = SH110X_BLACK;
-    else color = SH110X_WHITE;
-    display.display();
-    delay(1);
-  }
-}
-
-void testdrawroundrect(void) {
-  for (int16_t i = 0; i < display.height() / 2 - 2; i += 2) {
-    display.drawRoundRect(i, i, display.width() - 2 * i, display.height() - 2 * i, display.height() / 4, SH110X_WHITE);
-    display.display();
-    delay(1);
-  }
-}
-
-void testfillroundrect(void) {
-  uint8_t color = SH110X_WHITE;
-  for (int16_t i = 0; i < display.height() / 2 - 2; i += 2) {
-    display.fillRoundRect(i, i, display.width() - 2 * i, display.height() - 2 * i, display.height() / 4, color);
-    if (color == SH110X_WHITE) color = SH110X_BLACK;
-    else color = SH110X_WHITE;
-    display.display();
-    delay(1);
-  }
-}
-
-void testdrawrect(void) {
-  for (int16_t i = 0; i < display.height() / 2; i += 2) {
-    display.drawRect(i, i, display.width() - 2 * i, display.height() - 2 * i, SH110X_WHITE);
-    display.display();
-    delay(1);
-  }
-}
-
-void testdrawline() {
-  for (int16_t i = 0; i < display.width(); i += 4) {
-    display.drawLine(0, 0, i, display.height() - 1, SH110X_WHITE);
-    display.display();
-    delay(1);
-  }
-  for (int16_t i = 0; i < display.height(); i += 4) {
-    display.drawLine(0, 0, display.width() - 1, i, SH110X_WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-
-  display.clearDisplay();
-  for (int16_t i = 0; i < display.width(); i += 4) {
-    display.drawLine(0, display.height() - 1, i, 0, SH110X_WHITE);
-    display.display();
-    delay(1);
-  }
-  for (int16_t i = display.height() - 1; i >= 0; i -= 4) {
-    display.drawLine(0, display.height() - 1, display.width() - 1, i, SH110X_WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-
-  display.clearDisplay();
-  for (int16_t i = display.width() - 1; i >= 0; i -= 4) {
-    display.drawLine(display.width() - 1, display.height() - 1, i, 0, SH110X_WHITE);
-    display.display();
-    delay(1);
-  }
-  for (int16_t i = display.height() - 1; i >= 0; i -= 4) {
-    display.drawLine(display.width() - 1, display.height() - 1, 0, i, SH110X_WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-
-  display.clearDisplay();
-  for (int16_t i = 0; i < display.height(); i += 4) {
-    display.drawLine(display.width() - 1, 0, 0, i, SH110X_WHITE);
-    display.display();
-    delay(1);
-  }
-  for (int16_t i = 0; i < display.width(); i += 4) {
-    display.drawLine(display.width() - 1, 0, i, display.height() - 1, SH110X_WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
 }
